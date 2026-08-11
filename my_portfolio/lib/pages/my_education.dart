a5d21b51bda9252e2d6713923b635c1a9760a5ff
CharloLemba <carlolemba98@gmail.com>
2026-08-11 09:09:26 +0800

    update: menambahkan scroll dan atur ulang cards

--- my_portfolio/lib/pages/my_education.dart
+++ my_portfolio/lib/pages/my_education.dart
@@ -6,50 +6,50 @@ class MyEducation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "myEducation",
-              style: TextStyle(fontSize: 30),
-              textAlign: TextAlign.justify,
-            ),
-            Divider(
-              color: Colors.blueGrey,
-              thickness: 1,
-              height: 24,
-              indent: 16,
-              endIndent: 16,
-            ),
-            SizedBox(height: 20),
-            Cards(
-              teks1: "Elementry School",
-              teks2: "SD NEGERI 2 FONTEIN",
-              teks3: "2003 - 2009",
-            ),
-            SizedBox(height: 15),
-            Cards(
-              teks1: "Junior High School",
-              teks2: "SMP NEGERI 2 KUPANG",
-              teks3: "2009 - 2012",
-            ),
-            SizedBox(height: 15),
-            Cards(
-              teks1: "Senior High School",
-              teks2: "SMA NEGERI 1 KUPANG (Natural Science Major)",
-              teks3: "2012 - 2015",
-            ),
-            SizedBox(height: 15),
-            Cards(
-              teks1: "Bachelor's Degree",
-              teks2: "UNIKA WIDYA MANDIRA KUPANG (Computer Science Major)",
-              teks3: "2015 - 2022",
-            ),
-          ],
+    return SingleChildScrollView (
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Text(
+                "myEducation",
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.justify,
+              ),
+              Divider(
+                color: Colors.blueGrey,
+                thickness: 1,
+                height: 24,
+                indent: 16,
+                endIndent: 16,
+              ),
+              SizedBox(height: 20),
+              Cards(
+                teks1: "Elementry School",
+                teks2: "SD NEGERI 2 FONTEIN",
+                teks3: "2003 - 2009",
+              ),
+              SizedBox(height: 15),
+              Cards(
+                teks1: "Junior High School",
+                teks2: "SMP NEGERI 2 KUPANG",
+                teks3: "2009 - 2012",
+              ),
+              SizedBox(height: 15),
+              Cards(
+                teks1: "Senior High School",
+                teks2: "SMA NEGERI 1 KUPANG (Natural Science Major)",
+                teks3: "2012 - 2015",
+              ),
+              SizedBox(height: 15),
+              Cards(
+                teks1: "Bachelor's Degree",
+                teks2: "UNIKA WIDYA MANDIRA KUPANG (Computer Science Major)",
+                teks3: "2015 - 2022",
+              ),
+            ],
+          ),
         ),
       ),
     );
--- my_portfolio/lib/pages/my_experience.dart
+++ my_portfolio/lib/pages/my_experience.dart
@@ -6,38 +6,38 @@ class MyExperience extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "myExperience",
-              style: TextStyle(fontSize: 30),
-              textAlign: TextAlign.justify,
-            ),
-            Divider(
-              color: Colors.blueGrey,
-              thickness: 1,
-              height: 24,
-              indent: 16,
-              endIndent: 16,
-            ),
-            SizedBox(height: 20),
-            Cards(
-              teks1: "Marketing Executive",
-              teks2: "PT. SURYA MAHKOTA KENCANA",
-              teks3: "2023",
-            ),
-            SizedBox(height: 15),
-            Cards(
-              teks1: "Spare Parts Administrator",
-              teks2: "PT. SURYA MAHKOTA KENCANA",
-              teks3: "2023 - present",
-            ),
-          ],
+    return SingleChildScrollView(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Text(
+                "myExperience",
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.justify,
+              ),
+              Divider(
+                color: Colors.blueGrey,
+                thickness: 1,
+                height: 24,
+                indent: 16,
+                endIndent: 16,
+              ),
+              SizedBox(height: 20),
+              Cards(
+                teks1: "Marketing Executive",
+                teks2: "PT. SURYA MAHKOTA KENCANA",
+                teks3: "2023",
+              ),
+              SizedBox(height: 15),
+              Cards(
+                teks1: "Spare Parts Administrator",
+                teks2: "PT. SURYA MAHKOTA KENCANA",
+                teks3: "2023 - present",
+              ),
+            ],
+          ),
         ),
       ),
     );
